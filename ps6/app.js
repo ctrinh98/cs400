bbf3ea0363cee59a0df33a0795e13cc375dd8167
Christopher Trinh <ctrinh@bu.edu>
2019-11-26 06:15:12 -0500

    Replace ps6 app with newly refactored ps4 app

--- ps6/app.js
+++ ps6/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const ps4Router = require('./routes/ps4')
+const ps6Router = require('./routes/ps6');
+const requestRouter = require('./routes/request');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/ps4', ps4Router)
+app.use('/ps6', ps6Router);
+app.use('/request', requestRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- ps6/routes/index.js
+++ ps6/routes/index.js
@@ -3,16 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pokémon Yellow Pages' });
 });
 
-router.post('/ps4d', function(req,res){
+router.post('/ps6d', function(req,res){
   console.log(req.body);
   // console.log('please redirect');
   var string = encodeURIComponent(req.body.pokemon_name);
   console.log(req.body.pokemon_name);
   console.log(string);
-  return res.redirect('/ps4?name=' + string);
+  return res.redirect('/ps6?name=' + string);
 })
 
 module.exports = router;
